fc84365505c172e791e9afc1a45b1241655ec088
chihatw <chiha.tw@gmail.com>
2023-09-25 11:13:06 +0900

    pinyin feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "server only";
 
+import { PinyinList } from "@/features/pinyin";
+
 export default async function Home() {
   return (
     <main className="mx-[10vw] w-[calc(100%-20vw)] py-28 sm:mx-auto sm:w-[min(800px,100%-120px)]">
-      <div className="grid  place-items-center gap-y-8 "></div>
+      <div className="grid  place-items-center gap-y-8 ">
+        <PinyinList />
+      </div>
     </main>
   );
 }
